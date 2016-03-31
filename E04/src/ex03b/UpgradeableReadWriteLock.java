668ddef2519a5ceb5f320fc7836b4f3995bd0d11
Phil Schilter <phil.schilter@gmail.com>
2016-03-31 16:37:31 +0200

    add exercises for E04

---
+++ E04/src/ex03b/UpgradeableReadWriteLock.java
@@ -0,0 +1,29 @@
+package ex03b;
+
+public class UpgradeableReadWriteLock {
+  // TODO: Implement
+
+  public void readLock() throws InterruptedException {
+    // TODO:
+  }
+
+  public void readUnlock() {
+    // TODO:
+  }
+
+  public void upgradeableReadLock() throws InterruptedException {
+    // TODO:
+  }
+
+  public void upgradeableReadUnlock() {
+    // TODO:
+  }
+
+  public void writeLock() throws InterruptedException {
+    // TODO:
+  }
+
+  public void writeUnlock() {
+    // TODO:
+  }
+}
